4a0b71ebd55e8aaf00b3f8f6556a1bc9af5854a3
Cofy43 <cofy43@ciencias.unam.mx>
2019-11-25 16:53:58 -0600

    validacion del archivo xml a traves del archivo .dtd

--- src/Lectura/Lectura.java
+++ src/Lectura/Lectura.java
@@ -1,24 +1,55 @@
 package Lectura;
 
-import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 public class Lectura {
-
-    public void leeArchivo(String direccion) throws FileNotFoundException, IOException{
-        String l;
-        FileReader fr = new FileReader(direccion);
-        BufferedReader br = new BufferedReader(fr);
-        while ((l = br.readLine()) != null) {
-            System.out.println(l);
+    public static void main(String[] args) {
+        boolean flag = true;
+        try {
+            validate("/home/martin/Documents/Proyecto-Datos/redat.xml");
+        } catch (ParserConfigurationException e) {
+            flag = false;
+        } catch (FileNotFoundException e) {
+            flag = false;
+        } catch (SAXException e) {
+            flag = false;
+        } catch (IOException e) {
+            flag = false;
         }
-        br.close();
+        String respuesta = "";
+        if (flag) {
+            respuesta = "valido";
+        } else {
+            respuesta = "invalido";
+        }
+        System.out.println("el archivo es " + respuesta);
     }
 
-    public static void main(String[] args) throws IOException{
-        Lectura lec = new Lectura();
-        lec.leeArchivo("/home/martin/Documents/Proyecto-Datos/redat.xml");
+    public static void validate(String xmlString)
+            throws ParserConfigurationException, FileNotFoundException, SAXException, IOException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setValidating(true);
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        db.setErrorHandler(new org.xml.sax.ErrorHandler() {
+            @Override
+            public void error(SAXParseException arg0) throws SAXException {
+            }
+
+            @Override
+            public void fatalError(SAXParseException arg0) throws SAXException {
+            }
+
+            @Override
+            public void warning(SAXParseException arg0) throws SAXException {
+            }
+        });
+        db.parse(new FileInputStream(xmlString));
     }
 }
